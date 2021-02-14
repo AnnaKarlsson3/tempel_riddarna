8d8ba4b370deaaf081ea98d5e4749e9398f257e9
AnnaKarlsson3 <43141496+AnnaKarlsson3@users.noreply.github.com>
2021-02-14 21:32:56 +0100

    wip home

--- frontend_tempel_riddarna/src/pages/Home.js
+++ frontend_tempel_riddarna/src/pages/Home.js
@@ -1,40 +1,47 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, Navbar, Nav, NavbarBrand, NavLink, NavItem } from "reactstrap";
+import { NavLink as RRNavLink, Link } from "react-router-dom";
 
 export default function Home() {
+
+
+
+
     return (
-      <Container fluid={true}>
-        <Row>
-          <Col lg="8" md="12" sm="12">
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-            <p>Nått här?</p>
-          </Col>
-          <Col lg="4" md="12" sm="12">
-            <p>Nått här?</p>
-          </Col>
+      <Container fluid={true} className="image">
+       <Row>
+           <Col className="free-space"></Col>
+           </Row>  
+        <Row className="nav-row">
+
+            <Navbar className="container-fluid , navbar-main">
+            <NavbarBrand className="nav-brand" href="/">ICON</NavbarBrand>
+                <Nav >
+                    <NavItem className="ml-auto">
+                        <NavLink className="nav-link" tag={Link} to={"/"}>
+                            Files |
+                        </NavLink>
+                    </NavItem>
+                    <NavItem className="ml-auto">
+                        <NavLink className="nav-link" tag={Link} to={"/"}>
+                            Gallery |
+                        </NavLink>
+                    </NavItem>
+                    <NavItem className="ml-auto">
+                        <NavLink className="nav-link" tag={Link} to={"/"}>
+                            Memes |
+                        </NavLink>
+                    </NavItem>
+                    <NavItem className="ml-auto">
+                        <NavLink className="nav-link" tag={Link} to={"/"}>
+                            Forum 
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+
+             </Navbar>
         </Row>
+       
       </Container>
     );
   }
